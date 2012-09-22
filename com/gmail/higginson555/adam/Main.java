9558aa0c3ec95513afa3bbfedf893477c1d4d68d
adam.higginson555 <adam.higginson555@a31323f1-0baa-5575-0c4c-2d0e6625d0e8>
2012-09-22 12:13:27 +0000

    Added Main Class

---
+++ com/gmail/higginson555/adam/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmail.higginson555.adam;
+
+/**
+ *
+ * @author Adam
+ */
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Started ultimate e-mail client...");
+    }
+    
+}
