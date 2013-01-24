41bbad592d7703583a7d4943353684bd734ad49f
adam.higginson555 <adam.higginson555@a31323f1-0baa-5575-0c4c-2d0e6625d0e8>
2013-01-24 14:51:17 +0000

    Updated the adding of folders

--- trunk/com/gmail/higginson555/adam/AccountMessageDownloader.java
+++ trunk/com/gmail/higginson555/adam/AccountMessageDownloader.java
@@ -59,6 +59,215 @@ public class AccountMessageDownloader
         }
     }
     
+    /*
+     * Recrusive function to insert folders into the database.
+     * parentID - The ID of the current folder's parent, -1 if no parent
+     * folder - The current folder
+     */
+    private void insertFolderIntoDatabase(int parentID, Folder folder) throws MessagingException,
+                                                    SQLException
+    {
+        //Insert this node into the database, with the parentID
+        Database user = UserDatabase.getInstance();
+        ArrayList<Object[]> result;
+        //ParentID -1 when no parent
+        if (parentID == -1)
+        {
+            result = user.selectFromTableWhere("Folders", "folderID", 
+                    "name='" + folder.getName() 
+                    + "' AND parentFolder IS NULL "
+                    + "AND accountUsername='" + account.getUsername() + "'");
+        }
+        else
+        {
+            result = user.selectFromTableWhere("Folders", "folderID", 
+                "name='" + folder.getName() + 
+                "' AND parentFolder=" + Integer.toString(parentID) + 
+                " AND accountUsername='" + account.getUsername() + "'");
+        }
+            
+        //If result it empty, folder does not already exist in database, insert it
+        if (result.isEmpty())
+        {
+            //ParentID is -1 when no parent
+            if (parentID == -1)
+            {
+                
+                String[] fieldNames = {"name", "accountUsername"};
+                Object[] fieldValues = {folder.getName(), account.getUsername()};
+                user.insertRecord("Folders", fieldNames, fieldValues);
+            }
+            else //parent found
+            {
+                String[] fieldNames = {"name", "accountUsername", "parentFolder"};
+                Object[] fieldValues = {folder.getName(), account.getUsername(), parentID};
+                user.insertRecord("Folders", fieldNames, fieldValues);
+            }
+        }
+        
+        System.out.println("Now on folder: " + folder.getFullName());
+        
+        //Find this folder ID
+        if (parentID == -1)
+        {
+            result = user.selectFromTableWhere("Folders", "folderID", 
+                "name='" + folder.getName() + 
+                "' AND parentFolder IS NULL" +
+                " AND accountUsername='" + account.getUsername() + "'");
+        }
+        else
+        {
+            result = user.selectFromTableWhere("Folders", "folderID", 
+                "name='" + folder.getName() + 
+                "' AND parentFolder=" + Integer.toString(parentID) + 
+                " AND accountUsername='" + account.getUsername() + "'");
+        }
+        
+        //Should always be full, if not an error has occurred
+        //And should only be one result
+        int id = (Integer) result.get(0)[0];
+        
+        //Need to recurse down tree if more folders
+        if (folder.getType() == Folder.HOLDS_FOLDERS)
+        {
+            //Recurse for each child
+            Folder[] children = folder.list();
+            for (Folder child : children)
+            {
+                insertFolderIntoDatabase(id, child);
+            }  
+        }
+        
+        insertMessagesInFolder(id, folder);
+    }
+    
+    private void insertMessagesInFolder(int folderID, Folder folder) 
+            throws MessagingException, 
+                   SQLException
+    {
+        if ((folder.getType() != Folder.HOLDS_FOLDERS) && !folder.isOpen())
+        {
+            folder.open(Folder.READ_ONLY);
+        }
+        else {
+            return;
+        }
+        
+        //Get all messages in this folder
+        Message[] allMessages = folder.getMessages();
+        
+        if (allMessages.length == 0)
+        {
+            return;
+        }
+
+        FetchProfile fp = new FetchProfile();
+        fp.add(FetchProfile.Item.ENVELOPE);
+        fp.add(FetchProfile.Item.FLAGS);
+        fp.add(UIDFolder.FetchProfileItem.UID);
+        //fp.add(FetchProfile.Item.CONTENT_INFO);
+        fp.add("X-mailer");
+
+        folder.fetch(allMessages, fp);
+
+        ArrayList<Object[]> dbData = new ArrayList<Object[]>(allMessages.length); 
+        for (int i = allMessages.length - 1; i >= 0; i--)
+        {
+            String subject = "";
+            String from = "";
+            subject += allMessages[i].getSubject();
+            Address[] addresses = allMessages[i].getFrom();
+            from += addresses[0].toString();
+            String to = "";
+            //This bit here is slow for some reason?
+            /*addresses = allMessages[i].getAllRecipients();
+            if (addresses != null)
+            {
+                for (int j = 0; j < addresses.length - 1; j++)
+                {
+                    to += addresses[j].toString() + ",";
+                }
+                //So we don't add a comma at the end
+                to += addresses[addresses.length - 1];
+            }*/
+            String UID = "";
+            if (account.getAccountType().equalsIgnoreCase("IMAP") || account.getAccountType().equalsIgnoreCase("IMAPS"))
+            {
+                IMAPFolder imapFolder = (IMAPFolder) folder;
+                UID = Long.toString(imapFolder.getUID(allMessages[i]));
+            }
+            else if (account.getAccountType().equalsIgnoreCase("POP3"))
+            {
+                POP3Folder pop3Folder = (POP3Folder) folder;
+                UID = pop3Folder.getUID(allMessages[i]);
+            }    
+
+            Date dateSent = allMessages[i].getSentDate();
+            Date dateReceived = allMessages[i].getReceivedDate();
+            Object[] line = {UID, subject, from, to, dateSent, dateReceived, folderID};
+            dbData.add(line);
+        }
+
+        Comparator<Object[]> messageDataComp = new Comparator<Object[]>()
+        {
+
+            @Override
+            public int compare(Object[] o1, Object[] o2) 
+            {
+                Date d1 = (Date) o1[5];
+                Date d2 = (Date) o2[5];
+
+                return d2.compareTo(d1);
+            }
+
+        };
+
+        Collections.sort(dbData, messageDataComp);
+
+        FolderManager fm = new FolderManager(UserDatabase.getInstance());
+        Date lastSetDate = fm.getLastDate(folderID);
+        //System.out.println("\nLast set date: " + lastSetDate);
+
+        Iterator<Object[]> dataIter = dbData.iterator();
+        ArrayList<Object[]> dbDataToAdd = new ArrayList<Object[]>();
+        boolean isUpdatingDB = false;
+        while (dataIter.hasNext())
+        {
+            Object[] currentLine = dataIter.next();
+            Date foundDate = (Date) currentLine[5];
+            //System.out.println("Found date: " + foundDate);
+
+            //If we've found a message which was sent after the last set
+            //date, we need to update the database with the latest message data
+            if (lastSetDate == null || foundDate.after(lastSetDate))
+            {
+                dbDataToAdd.add(currentLine);
+                isUpdatingDB = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+
+        if (isUpdatingDB)
+        {
+            System.out.println("\n----UPDATING DATABASE WITH NEW MESSAGES!----\n");
+            fm.setLastDate(folderID, (Date) dbDataToAdd.get(0)[5]);
+            publishPropertyEvent("MessageManagerThreadStart", null);
+            MessageManager mm = new MessageManager(UserDatabase.getInstance());
+            for (PropertyListener listener : listeners)
+            {
+                mm.addListener(listener);
+            }
+            mm.addMessages(dbDataToAdd);
+            System.out.println("\n\n\n\n -------------- RETURNED, THREAD WORKED?! ------------------\n\n\n\n");
+        }
+        
+        folder.close(false);
+    }
+    
     /**
      * Gets the messages and begins adding them to the database.
      * Note, adding messages to the database is done on a seperate thread.
@@ -66,131 +275,10 @@ public class AccountMessageDownloader
     public void getMessages() throws MessagingException, SQLException
     {
         connectToServer();
-
-        Folder[] folders = store.getDefaultFolder().list();
-        for (Folder folder : folders)
-        {
-            int folderID = -1;
-            if ((folder.getType() != Folder.HOLDS_FOLDERS) && !folder.isOpen())
-            {
-                folder.open(Folder.READ_WRITE);
-            }
-            else
-            {
-                //TODO
-                continue;
-            }
-
-            //Add this folder to the database, working out its proper parent path!
-            folderID = addFolder(folder);
-
-            //Get all messages in this folder
-            Message[] allMessages = folder.getMessages();
-
-            FetchProfile fp = new FetchProfile();
-            fp.add(FetchProfile.Item.ENVELOPE);
-            fp.add(FetchProfile.Item.FLAGS);
-            fp.add(UIDFolder.FetchProfileItem.UID);
-            //fp.add(FetchProfile.Item.CONTENT_INFO);
-            fp.add("X-mailer");
-
-            folder.fetch(allMessages, fp);
-
-            ArrayList<Object[]> dbData = new ArrayList<Object[]>(allMessages.length); 
-            for (int i = allMessages.length - 1; i >= 0; i--)
-            {
-                String subject = "";
-                String from = "";
-                subject = allMessages[i].getSubject();
-                Address[] addresses = allMessages[i].getFrom();
-                from = addresses[0].toString();
-                String to = "";
-                //This bit here is slow for some reason?
-                /*addresses = allMessages[i].getAllRecipients();
-                if (addresses != null)
-                {
-                    for (int j = 0; j < addresses.length - 1; j++)
-                    {
-                        to += addresses[j].toString() + ",";
-                    }
-                    //So we don't add a comma at the end
-                    to += addresses[addresses.length - 1];
-                }*/
-                String UID = "";
-                if (account.getAccountType().equalsIgnoreCase("IMAP") || account.getAccountType().equalsIgnoreCase("IMAPS"))
-                {
-                    IMAPFolder imapFolder = (IMAPFolder) folder;
-                    UID = Long.toString(imapFolder.getUID(allMessages[i]));
-                }
-                else if (account.getAccountType().equalsIgnoreCase("POP3"))
-                {
-                    POP3Folder pop3Folder = (POP3Folder) folder;
-                    UID = pop3Folder.getUID(allMessages[i]);
-                }    
-
-                Date dateSent = allMessages[i].getSentDate();
-                Date dateReceived = allMessages[i].getReceivedDate();
-                Object[] line = {UID, subject, from, to, dateSent, dateReceived, folderID};
-                dbData.add(line);
-            }
-
-            Comparator<Object[]> messageDataComp = new Comparator<Object[]>()
-            {
-
-                @Override
-                public int compare(Object[] o1, Object[] o2) 
-                {
-                    Date d1 = (Date) o1[5];
-                    Date d2 = (Date) o2[5];
-
-                    return d2.compareTo(d1);
-                }
-
-            };
-
-            Collections.sort(dbData, messageDataComp);
-
-            FolderManager fm = new FolderManager(UserDatabase.getInstance());
-            Date lastSetDate = fm.getLastDate(folderID);
-            //System.out.println("\nLast set date: " + lastSetDate);
-
-            Iterator<Object[]> dataIter = dbData.iterator();
-            ArrayList<Object[]> dbDataToAdd = new ArrayList<Object[]>();
-            boolean isUpdatingDB = false;
-            while (dataIter.hasNext())
-            {
-                Object[] currentLine = dataIter.next();
-                Date foundDate = (Date) currentLine[5];
-                //System.out.println("Found date: " + foundDate);
-
-                //If we've found a message which was sent after the last set
-                //date, we need to update the database with the latest message data
-                if (lastSetDate == null || foundDate.after(lastSetDate))
-                {
-                    dbDataToAdd.add(currentLine);
-                    isUpdatingDB = true;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-
-            if (isUpdatingDB)
-            {
-                System.out.println("\n----UPDATING DATABASE WITH NEW MESSAGES!----\n");
-                fm.setLastDate(folderID, (Date) dbDataToAdd.get(0)[5]);
-                publishPropertyEvent("MessageManagerThreadStart", null);
-                MessageManager mm = new MessageManager(UserDatabase.getInstance());
-                for (PropertyListener listener : listeners)
-                {
-                    mm.addListener(listener);
-                }
-                mm.addMessages(dbDataToAdd);
-                System.out.println("\n\n\n\n -------------- RETURNED, THREAD WORKED?! ------------------\n\n\n\n");
-            }
-        }
+        
+        //Try to insert all folders into the database, as well as messages!
+        Folder root = store.getDefaultFolder();
+        insertFolderIntoDatabase(-1, root);
     }      
     
     private void connectToServer() throws MessagingException
@@ -211,50 +299,5 @@ public class AccountMessageDownloader
         Session session = Session.getInstance(properties);
         store = session.getStore(account.getAccountType().toLowerCase());
         store.connect(account.getIncoming(), account.getUsername(), decPass);
-    }
-    
-    private int addFolder(Folder folder)
-    {
-        int folderID = -1;
-                    
-        FolderManager folderManager = new FolderManager(UserDatabase.getInstance());
-
-        //Find out folder ids, and add them to database
-        Folder parent = null;
-        try 
-        {
-            parent = folder.getParent();
-            if (folder.getParent() == null || folder.getParent().getName().isEmpty())
-            {
-                folderID = folderManager.addFolder(folder.getName(), account);
-            }
-            else //Folder has parent, work out parent tree!
-            {
-                ArrayList<String> folderStack = new ArrayList<String>();
-                while (parent != null && !parent.getName().isEmpty())
-                {
-                    folderStack.add(parent.getName());
-                    System.out.println("Adding: " + parent.getName() + "@ " + folderStack.lastIndexOf(parent.getName()));
-                    parent = parent.getParent();
-                }
-
-                Collections.reverse(folderStack);
-                String[] parentFolderList = folderStack.toArray(new String[folderStack.size()]);
-                folderID = folderManager.addFolder(folder.getName(), parentFolderList, account);
-            }
-        } 
-        catch (MessagingException ex) 
-        {
-            Logger.getLogger(FolderNode.class.getName()).log(Level.SEVERE, null, ex);
-            System.exit(-1);
-        }
-        catch (SQLException ex)
-        {
-            ex.printStackTrace();
-            System.exit(-1);
-        }
-
-        return folderID;
-    }
-    
+    }        
 }
